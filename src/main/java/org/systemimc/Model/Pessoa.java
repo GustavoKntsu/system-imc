1134322f31af6dfb03e739d2251f93b9eaa09989
GustavoKntsu <gustavokntsu@gmail.com>
2025-10-08 15:23:18 -0300

    Adicionando mais coisas na classe Pessoa

--- src/main/java/org/systemimc/Model/Pessoa.java
+++ src/main/java/org/systemimc/Model/Pessoa.java
@@ -1,72 +1,71 @@
 package org.systemimc.Model;
 
 public class Pessoa {
+    private static int id = 0;
     private String nome;
-    private float altura;
-    private float peso;
-    private float imc;
+    private Float altura;
+    private Float peso;
+    private Float imc;
+    private String classificacao;
     //********************************************************************
     public Pessoa() {
-
+        this.id = ++this.id;
     }
 
-    public Pessoa(String nome, float altura, float peso, float imc) {
+    public Pessoa(int id, String nome, Float altura, Float peso, Float imc, String classificacao) {
         this.nome = nome;
         this.altura = altura;
         this.peso = peso;
         this.imc = imc;
+        this.classificacao = classificacao;
     }
 
-    public Pessoa(String nome, float altura, float peso) {
-        this.nome = nome;
-        this.altura = altura;
-        this.peso = peso;
-    }
-    //********************************************************************
-    public String getNome() {
-        return nome;
+    //***************************************************************
+    //Get & Set
+    public static int getId() {
+        return id;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public static void setId(int id) {
+        Pessoa.id = id;
     }
 
-    public float getAltura() {
-        return altura;
-    }
-
-    public void setAltura(float altura) {
+    public void setAltura(Float altura) {
         this.altura = altura;
     }
 
-    public float getPeso() {
-        return peso;
-    }
-
-    public void setPeso(float peso) {
+    public void setPeso(Float peso) {
         this.peso = peso;
     }
 
-    public float getImc() {
-        return imc;
-    }
-
-    public void setImc(float imc) {
+    public void setImc(Float imc) {
         this.imc = imc;
     }
 
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
     //********************************************************************
+
+
     @Override
     public String toString() {
         return "Pessoa{" +
                 "nome='" + nome + '\'' +
                 ", altura=" + altura +
                 ", peso=" + peso +
+                ", imc=" + imc +
+                ", classificacao='" + classificacao + '\'' +
                 '}';
     }
     //********************************************************************
-
-    public float calcularIMC() {
+    //Calculo IMC
+    public Float calcularIMC() {
         this.imc = this.peso / (this.altura * this.altura);
         return this.imc;
     }
@@ -75,18 +74,18 @@ public class Pessoa {
     public String classificacaoIMC() {
         String classificacao;
         if(this.imc < 18.5)
-            classificacao = "Abaixo do peso";
+            this.classificacao = "Abaixo do peso";
         else if(this.imc >= 18.5 && this.imc < 24.9)
-            classificacao = "Peso normal";
+            this.classificacao = "Peso normal";
         else if(this.imc >= 25 && this.imc < 29.9)
-            classificacao = "Sobrepeso";
+            this.classificacao = "Sobrepeso";
         else if(this.imc >= 30 && this.imc < 34.9)
-            classificacao = "Obesidade grau 1";
+            this.classificacao = "Obesidade grau 1";
         else if(this.imc >= 35 && this.imc < 39.9)
-            classificacao = "Obesidade grau 2";
+            this.classificacao = "Obesidade grau 2";
         else
             return "Obesidade grau 3";
 
-        return classificacao;
+        return this.classificacao;
     }
 }
