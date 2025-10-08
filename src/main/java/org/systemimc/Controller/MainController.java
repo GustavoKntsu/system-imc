e83d76b7d46ba23e0f511aeeef793cbf12fe76d6
GustavoKntsu <gustavokntsu@gmail.com>
2025-10-08 14:31:01 -0300

    Configuracoes da Controller

--- src/main/java/org/systemimc/Controller/MainController.java
+++ src/main/java/org/systemimc/Controller/MainController.java
@@ -2,13 +2,36 @@ package org.systemimc.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.systemimc.Model.Pessoa;
+
+import java.text.DecimalFormat;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    public TextField txtNome;
+    @FXML
+    public TextField txtAltura;
+    @FXML
+    public TextField txtPeso;
+    @FXML
+    public Label lblIMC;
+    @FXML
+    public Label lblClassificacao;
+
+    Pessoa pessoa = new Pessoa();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onCalcularIMCClick() {
+        DecimalFormat df = new DecimalFormat();
+        this.pessoa.setNome(this.txtNome.getText()) ;
+        this.pessoa.setAltura( Float.parseFloat(txtAltura.getText()));
+        this.pessoa.setPeso( Float.parseFloat(txtPeso.getText()));
+
+
+        df.applyPattern("#0.00");
+        this.lblIMC.setText(df.format(this.pessoa.calcularIMC()));
+        this.lblClassificacao.setText(this.pessoa.classificacaoIMC());
+
     }
 }
